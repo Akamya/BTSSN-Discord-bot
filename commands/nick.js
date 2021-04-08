ddd7a04c8aafdc7a3f166b385e775bd66eb39a63
Akamya <75986500+Akamya@users.noreply.github.com>
2021-04-08 13:20:38 +0200

    Update nick.js
    Added comments

--- commands/nick.js
+++ commands/nick.js
@@ -4,17 +4,18 @@ module.exports = {
 	run(message, args) {
 		let s = true;
 		var fNick = '';
+		/* concatenate the args (message content) */ 
 		try {
 			for (var i = 1; i < args.length; i++) {
 				fNick += args[i] + ' ';
-            }
+            	}
 			message.member.setNickname(fNick);
 		} catch (e) {
 			s = false;
-        }
-
+        	}
+		/* if success send a message to the user */ 
 		if (s) {
 			message.channel.send("Successfully changed your nickname to " + fNick);
-        }
+        	}
 	},
 };
